f083141e1e98e929cf6e5eaed54e98084dc62f69
sgh110110110 <babakjalilian1990@gmail.com>
2022-06-22 12:37:26 +0430

    fix lint error

--- src/Redux-Manager/index.ts
+++ src/Redux-Manager/index.ts
@@ -1,22 +1,34 @@
-import { Dispatch, IPuzzleDispatch, IReduxActions, IReduxActionTypes, IReduxState } from 'Redux-Manager/interfaces/puzzle.Interface';
-import { rdxActionTypes, rdxCreateWebSocketAndPuzzleAsync, rdxgoToNextLevelAsync, rdxReturnToWelcomeAsync, rdxRotatePuzzleCellOnClient, rdxRotatePuzzleCellsOnServer, rdxValidateExistingPuzzleAsync } from './actions/puzzleActions';
-
+import {
+  Dispatch,
+  IPuzzleDispatch,
+  IReduxActions,
+  IReduxActionTypes,
+  IReduxState
+} from 'Redux-Manager/interfaces/puzzle.Interface';
+import {
+  rdxActionTypes,
+  rdxCreateWebSocketAndPuzzleAsync,
+  rdxgoToNextLevelAsync,
+  rdxReturnToWelcomeAsync,
+  rdxRotatePuzzleCellOnClient,
+  rdxRotatePuzzleCellsOnServer,
+  rdxValidateExistingPuzzleAsync
+} from './actions/puzzleActions';
 
 export type {
-    IReduxActionTypes,
-    IReduxActions,
-    IReduxState,
-    IPuzzleDispatch,
-    Dispatch,
+  IReduxActionTypes,
+  IReduxActions,
+  IReduxState,
+  IPuzzleDispatch,
+  Dispatch,
 };
 export {
-    rdxActionTypes,
-    rdxReturnToWelcomeAsync,
-    rdxCreateWebSocketAndPuzzleAsync,
-    rdxRotatePuzzleCellOnClient,
-    rdxRotatePuzzleCellsOnServer,
-    rdxValidateExistingPuzzleAsync,
-    rdxgoToNextLevelAsync,
+  rdxActionTypes,
+  rdxReturnToWelcomeAsync,
+  rdxCreateWebSocketAndPuzzleAsync,
+  rdxRotatePuzzleCellOnClient,
+  rdxRotatePuzzleCellsOnServer,
+  rdxValidateExistingPuzzleAsync,
+  rdxgoToNextLevelAsync,
 };
-
 
